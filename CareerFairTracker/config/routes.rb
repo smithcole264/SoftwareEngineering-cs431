730c1fec5ae08ada71aa56cd17492b1e40be3e1f
clarkmyfancy <clark.jack0@gmail.com>
2019-10-22 22:10:09 -0500

    added routing and such

--- CareerFairTracker/config/routes.rb
+++ CareerFairTracker/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  # get 'welcome/index'
+
+  match ':controller(/:action(/:id))', :via => :get 
 
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
