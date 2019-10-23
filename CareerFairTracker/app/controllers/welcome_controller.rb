eb1a43bbb23f94c8ce0ef07a39adfa89e6da2c36
clarkmyfancy <clark.jack0@gmail.com>
2019-10-22 22:23:37 -0500

    helpful useful shit

--- CareerFairTracker/app/controllers/welcome_controller.rb
+++ CareerFairTracker/app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+    if (params.has_key?(:id) && params.has_key?(:random))
+      @id = params['id']
+      @whateverYouWant = params['random']
+    elsif (params.has_key?(:id))
+      @id = params['id']
+    end
   end
 
   def sample
