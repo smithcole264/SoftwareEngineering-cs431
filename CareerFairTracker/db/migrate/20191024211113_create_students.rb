8595516e9ed76f68719e20a88b25b2e297bd3256
clarkmyfancy <clark.jack0@gmail.com>
2019-10-24 17:51:20 -0500

    ran migration

--- CareerFairTracker/db/migrate/20191024211113_create_students.rb
+++ CareerFairTracker/db/migrate/20191024211113_create_students.rb
@@ -1,6 +1,11 @@
 class CreateStudents < ActiveRecord::Migration[6.0]
   def change
     create_table :students do |t|
+      t.string :first_name, :null => false
+      t.string :last_name, :null => false
+      t.string :major
+      t.string :expected_grad_month
+      t.integer :expected_grad_year
 
       t.timestamps
     end
