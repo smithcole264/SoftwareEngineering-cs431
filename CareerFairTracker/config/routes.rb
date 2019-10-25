5577f320d3f96c78a8a843b8e15b3349a92870fe
clarkmyfancy <clark.jack0@gmail.com>
2019-10-24 20:05:42 -0500

    updated routes

--- CareerFairTracker/config/routes.rb
+++ CareerFairTracker/config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   # get 'welcome/index'
 
   # resources :welcome
+  resources :students
+  resources :login
   ##### the above thing uses default methods like 'show' 'index(i tink idk)' 'create'
   ##### if you want to route by hand you have to keep the above commented out!
   match ':controller(/:action(/:id))', :via => :get 
+  match ':controller(/:action(/:id))', :via => :post
 
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
