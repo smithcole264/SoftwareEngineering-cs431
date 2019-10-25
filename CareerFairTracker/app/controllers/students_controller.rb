c7efe2a6abef3c22e5fba62cecc4dc8546d51929
clarkmyfancy <clark.jack0@gmail.com>
2019-10-25 01:33:47 -0500

    create method successfuly makes new students

--- CareerFairTracker/app/controllers/students_controller.rb
+++ CareerFairTracker/app/controllers/students_controller.rb
@@ -1,7 +1,7 @@
 class StudentsController < ApplicationController
-    # def student_params
-    #     params.require(:student).permit(:fn, :ln)
-    # end
+    def student_params
+        params.require(:student).permit(:first_name, :last_name, :major, :expected_grad_month, :expected_grad_year)
+    end
 
     def index
         @students = Student.all 
@@ -17,9 +17,9 @@ class StudentsController < ApplicationController
     end
 
     def create
-        @student = Student.create!(first_name: params[:fn], last_name: params[:ln])
+        @student = Student.create!(student_params)
         flash[:notice] = "student was attempted to be added"
-        redirect_to student_index_path
+        redirect_to students_path
     end
 
     def update
