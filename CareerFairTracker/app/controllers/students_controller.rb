6060fde2b7a73236a7896b245aa391cced4454e9
clarkmyfancy <clark.jack0@gmail.com>
2019-10-24 20:06:07 -0500

    attempting to create new student

--- CareerFairTracker/app/controllers/students_controller.rb
+++ CareerFairTracker/app/controllers/students_controller.rb
@@ -1,5 +1,15 @@
 class StudentsController < ApplicationController
+    # def student_params
+    #     params.require(:student).permit(:fn, :ln)
+    # end
+
     def index
         @students = Student.all 
+    end
+
+    def create
+        @student = Student.create!(first_name: params[:fn], last_name: params[:ln])
+        flash[:notice] = "student was attempted to be added"
+        redirect_to student_index_path
     end
 end
