0236c5662cc9f5dbe771a5276ea8b7db14103c57
clarkmyfancy <clark.jack0@gmail.com>
2019-10-28 19:54:29 -0500

    adding sessions controller

--- CareerFairTracker/config/routes.rb
+++ CareerFairTracker/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root 'home#index'
 
   match '/dashboards' => 'dashboards#index', via: :post
