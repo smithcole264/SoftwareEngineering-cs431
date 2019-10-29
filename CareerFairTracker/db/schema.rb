76393fa658922d04b8bcaf3c390621bc1137cee4
clarkmyfancy <clark.jack0@gmail.com>
2019-10-29 01:34:06 -0500

    changed from migrations

--- CareerFairTracker/db/schema.rb
+++ CareerFairTracker/db/schema.rb
@@ -22,13 +22,4 @@ ActiveRecord::Schema.define(version: 2019_10_29_001925) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-    t.string "user_type"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["username"], name: "index_users_on_username", unique: true
-  end
-
 end
