b6d2dff17467a6ff53501f0aa6802d58fe8f2c0c
clarkmyfancy <clark.jack0@gmail.com>
2019-10-28 19:12:21 -0500

    deleted files

--- CareerFairTracker/db/migrate/20191027222341_remove_non_null_contraints_from_user.rb
+++
@@ -1,7 +0,0 @@
-class RemoveNonNullContraintsFromUser < ActiveRecord::Migration[6.0]
-  def change
-    change_column_null :users, :user_type, true
-    change_column_null :users, :username, true
-    change_column_null :users, :password, true
-  end
-end
