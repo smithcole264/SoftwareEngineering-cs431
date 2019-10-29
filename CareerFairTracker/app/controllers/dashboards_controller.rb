17abf7fa414d1d837f59d42662b420c0a736273b
clarkmyfancy <clark.jack0@gmail.com>
2019-10-29 01:36:30 -0500

    beginning stage of creating user from signup page

--- CareerFairTracker/app/controllers/dashboards_controller.rb
+++ CareerFairTracker/app/controllers/dashboards_controller.rb
@@ -3,47 +3,11 @@ class DashboardsController < ApplicationController
     #     params.require(:login).permit(:user_type, :username, :password)
     # end
 
-    # def index 
-    #     @login_message = "Login"
-    # end
-
-    # def loginAttemptSucceeds(username, password)
-    #     if User.isVerified(username, password)
-    #         return true
-    #     end
-    #     return false
-    # end
-
     def index
-        @login_message = ""
-        if params[:bypass]
-            @_username = 'teamMember'
-            @_password = 'password'
-        else 
-            @_username = params[:login][:username]
-            @_password = params[:login][:password]
+        if params.has_key?(:user_type) && params.has_key?(:username) && params.has_key?(:password)
+            User.create!({user_type: params[:user_type], username: params[:username], password: params[:password]})
+            
         end
-
-        # if loginAttemptSucceeds(@_username, @_password) 
-        if User.isVerified(@_username, @_password)
-            @login_message = "User #{@_username} logged in."
-        end
-
-        # if params[:bypass]
-        #     @isValidUser = true
-        #     @login_message = "Bypassed login, Welcome"
-        # else
-        #     @username = params[:login][:username]
-        #     @password = params[:login][:password]
-
-        #     @isValidUser = verifyUsernameAndPassword(@username, @password)
-        #     if @isValidUser
-        #         @login_message = "Welcome #{@username}"
-        #     else
-        #         @login_error_message = "Login Failed"
-        #         redirect_to controller: :logins, action: :index, login_error_message: @login_error_message
-        #     end
-        # end
     end
 
     def show
