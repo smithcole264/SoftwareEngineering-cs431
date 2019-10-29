dbd79e16ebe833696824066e37bc86b9441e5173
clarkmyfancy <clark.jack0@gmail.com>
2019-10-29 01:33:50 -0500

    add helper function to keep track of current user

--- CareerFairTracker/app/controllers/application_controller.rb
+++ CareerFairTracker/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+    def current_user
+        puts "inside current user method"
+        if session[:user_id]
+            @current_user ||= User.find(session[:user_id])
+        else
+            @current_user = nil
+        end
+    end
 end
