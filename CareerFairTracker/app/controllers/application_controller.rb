4b542d0f3cc24ce3b6720204d25c7f4ddd6a0983
clarkmyfancy <clark.jack0@gmail.com>
2019-10-29 21:58:07 -0500

    added helpter method to get curent user

--- CareerFairTracker/app/controllers/application_controller.rb
+++ CareerFairTracker/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user
+
     def current_user
-        puts "inside current user method"
         if session[:user_id]
             @current_user ||= User.find(session[:user_id])
         else
