07c27ce02fc8c62a1152119914c8092f9dbb90b0
clarkmyfancy <clark.jack0@gmail.com>
2019-10-29 21:55:50 -0500

    creates user (or it doesnt) and reroutes

--- CareerFairTracker/app/controllers/users_controller.rb
+++ CareerFairTracker/app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def user_params
+    params.require(:user).permit(:user_type, :username, :password)
+  end
+
   def index
   end
 
@@ -12,6 +17,13 @@ class UsersController < ApplicationController
   end
 
   def create
+      @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = user.id 
+        redirect_to dashboard_url
+      else
+        redirect_to root_url, notice: "User Could not be created."
+      end   
   end
 
   def update
