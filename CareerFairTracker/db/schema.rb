377a5fa3aea547abc204ca2c9efed2e5dff4d34e
clarkmyfancy <clark.jack0@gmail.com>
2019-10-29 21:53:21 -0500

    migration auto updated this

--- CareerFairTracker/db/schema.rb
+++ CareerFairTracker/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_001925) do
+ActiveRecord::Schema.define(version: 2019_10_30_000131) do
 
   create_table "students", force: :cascade do |t|
     t.string "first_name", null: false
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2019_10_29_001925) do
     t.integer "expected_grad_year"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.string "user_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
