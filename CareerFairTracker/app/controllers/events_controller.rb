9fc44b72bfb13e4b93a86cabbbdba15b8bed0ba5
clarkmyfancy <clark.jack0@gmail.com>
2019-11-27 21:29:07 -0600

    possible to download attendance now

--- CareerFairTracker/app/controllers/events_controller.rb
+++ CareerFairTracker/app/controllers/events_controller.rb
@@ -1,33 +1,34 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
-  # GET /events
-  # GET /events.json
   def index
     @events = Event.all
   end
 
   def show
     @students = Student.all
+    @event = Event.find(params[:id])
+    @urlString = "#{@event.id}.csv"
+    respond_to do |format|
+      format.html
+      format.csv { send_data @students.to_csv}
+    end
+    
   end
 
-  # GET /events/new
   def new
     @event = Event.new
   end
 
-  # GET /events/1/edit
   def edit
   end
 
-  # POST /events
-  # POST /events.json
   def create
     @event = Event.new(event_params)
 
     respond_to do |format|
       if @event.save
-        format.html { redirect_to dashboards_path, notice: 'Event was successfully created.' }
+        format.html { redirect_to admin_dashboard_path, notice: 'Event was successfully created.' }
         format.json { render :show, status: :created, location: @event }
       else
         format.html { render :new }
@@ -36,8 +37,6 @@ class EventsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
   def update
     respond_to do |format|
       if @event.update(event_params)
@@ -50,8 +49,6 @@ class EventsController < ApplicationController
     end
   end
 
-  # DELETE /events/1
-  # DELETE /events/1.json
   def destroy
     @event.destroy
     respond_to do |format|
@@ -66,7 +63,6 @@ class EventsController < ApplicationController
       @event = Event.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
       params.require(:event).permit(:name, :event_date, :start_time, :end_time)
     end
